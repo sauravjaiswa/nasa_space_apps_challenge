257bc1337c7c647aecea83f2afdfcbc9a01a1935
Saurav Jaiswal <sauravjaiswal520@gmail.com>
2019-10-15 12:01:46 +0530

    test
    push1

--- Android/app/src/main/java/com/example/nasa/Check_Safety.java
+++ Android/app/src/main/java/com/example/nasa/Check_Safety.java
@@ -2,27 +2,34 @@
 
         import android.Manifest;
         import android.annotation.SuppressLint;
+        import android.content.Context;
         import android.content.Intent;
         import android.content.pm.PackageManager;
         import android.location.Location;
         import android.location.LocationListener;
         import android.location.LocationManager;
+        import android.net.Uri;
         import android.os.Build;
         import android.os.Bundle;
         import android.provider.Settings;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
+        import android.widget.Toast;
 
         import androidx.annotation.NonNull;
         import androidx.annotation.Nullable;
         import androidx.appcompat.app.AppCompatActivity;
         import androidx.core.app.ActivityCompat;
+        import androidx.core.content.FileProvider;
+
+        import java.io.File;
+        import java.io.FileOutputStream;
 
         public class Check_Safety extends AppCompatActivity {
 
             private Button b;
-            private TextView t;
+            private TextView data;
             private LocationManager locationManager;
             private LocationListener listener;
 
@@ -33,8 +40,9 @@
 
                 setContentView(R.layout.activity_check__safety);
 
-                t = (TextView) findViewById(R.id.textView);
+                data = (TextView) findViewById(R.id.textView);
                 b = (Button) findViewById(R.id.button);
+                double t1,t2;
 
                 locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
@@ -42,7 +50,29 @@
                 listener = new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
-                        t.append("\n " + location.getLatitude() + " " + location.getLongitude());
+                        data.append("\n " + location.getLatitude() + "," + location.getLongitude());
+                        try{
+                            //saving the file into device
+                            FileOutputStream out = openFileOutput("data.txt", Context.MODE_PRIVATE);
+                            out.write((data.toString()).getBytes());
+                            out.close();
+                            Toast.makeText(Check_Safety.this,"Saved!",Toast.LENGTH_SHORT).show();
+
+                            //exporting
+                           /* Context context = getApplicationContext();
+                            File filelocation = new File(getFilesDir(), "data.csv");
+                            Uri path = FileProvider.getUriForFile(context, "com.example.nasa", filelocation);
+                            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+                            fileIntent.setType("text/csv");
+                            fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+                            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+                            startActivity(Intent.createChooser(fileIntent, "Send mail"));*/
+                        }
+                        catch(Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(Check_Safety.this,"Error!",Toast.LENGTH_SHORT).show();
+                        }
                     }
 
                     @Override
@@ -96,4 +126,28 @@
                     }
                 });
             }
+
+            /*public void export(View view)
+            {
+                try{
+                    //saving the file into device
+                    FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
+                    out.write((data.toString()).getBytes());
+                    out.close();
+
+                    //exporting
+                    Context context = getApplicationContext();
+                    File filelocation = new File(getFilesDir(), "data.csv");
+                    Uri path = FileProvider.getUriForFile(context, "com.example.nasa", filelocation);
+                    Intent fileIntent = new Intent(Intent.ACTION_SEND);
+                    fileIntent.setType("text/csv");
+                    fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+                    startActivity(Intent.createChooser(fileIntent, "Send mail"));
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }*/
         }
